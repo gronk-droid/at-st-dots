7dcc687cccf63f4735b4ed6afbda1fb31844774f
gronk-droid <gronk.droid@gmail.com>
2026-01-23 16:59:31 -0500

    feat(raycast): add yabai toggle float script

---
+++ .config/raycast/scripts/yabai-toggle-float.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title yabai-toggle-float
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🎈
+
+# Documentation:
+# @raycast.description Toggle window between tiled and floating
+# @raycast.author gronk-droid
+# @raycast.authorURL github.com/gronk-droid
+
+yabai -m window --toggle float
+
+
+
+
+
+
+
+
+
